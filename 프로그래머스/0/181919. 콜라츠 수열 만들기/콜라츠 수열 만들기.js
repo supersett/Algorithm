52f39d6343ceb380d301240ecf9842242b0e7c95
Hyunwoo Jung <92968138+supersett@users.noreply.github.com>
2025-08-09 14:57:28 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = [];
+    answer.push(n)
+    while(n!==1){
+        if(n%2===0){
+            n=n/2
+            answer.push(n)
+        }else{
+            n=(3*n)+1
+            answer.push(n)
+        }
+    }
+    return answer;
+}
